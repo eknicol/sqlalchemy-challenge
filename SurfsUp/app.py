9fbc4f08636949ac0fb7b1fec8373b53ef0773cb
Erin Nicol <erinknicol@gmail.com>
2024-05-13 23:04:29 +1000

    commit assignment

---
+++ SurfsUp/app.py
@@ -0,0 +1,45 @@
+# Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+
+# Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+
+# Declare a Base using `automap_base()`
+Base = automap_base()
+# Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
+
+
+# Assign the measurement class to a variable called `Measurement` and
+# the station class to a variable called `Station`
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+
